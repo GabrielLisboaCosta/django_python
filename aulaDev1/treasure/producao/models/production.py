eb22bb0fd0bb8b4064412fab43d7bfd656686cf6
Gabriel <gabrielcosta1055@hotmail.com>
2024-04-08 21:19:33 -0300

    att

--- aulaDev1/treasure/producao/models/production.py
+++ aulaDev1/treasure/producao/models/production.py
@@ -1,5 +1,6 @@
 from producao.models.base import *
 from django.core.validators import MinLengthValidator
+from django.db import models
 
 
 class Production(BaseModel):
@@ -8,23 +9,23 @@ class Production(BaseModel):
     class Meta:
         abstract = False
 
-    # title = models.CharField(max_length=100, validators=[MinLengthValidador(1)])
-    #
-    # advisory_certificate = models.CharField(max_length=10, validators=[MinLengthValidador(1)])
-    #
-    # lengh = models.DurationField()
-    #
-    # #genre =
-    #
-    # tredimenter = models.IntergerField()
-    #
-    # reviews = models.FloatField()
-    #
-    # specs = models.CharField(max_length=50, validators=[MinLengthValidador(5)])
-    #
-    # sinopse = models.CharField(max_length=500, validators=[MinLengthValidador(10)])
-    #
-    # realease_date = models.DateFiled()
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(1)])
+
+    advisory_certificate = models.CharField(max_length=10, validators=[MinLengthValidator(1)])
+
+    lengh = models.DurationField()
+
+    #genre =
+
+    tredimenter = models.IntegerField()
+
+    reviews = models.FloatField()
+
+    specs = models.CharField(max_length=50, validators=[MinLengthValidator(5)])
+
+    sinopse = models.CharField(max_length=500, validators=[MinLengthValidator(10)])
+
+    realease_date = models.DateField()
 
     def __str__(self):
         return self.title
