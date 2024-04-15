ff16485ca8f0962dfcd2bd67ee7cc4ddaf043b9b
Gabriel <gabrielcosta1055@hotmail.com>
2024-04-15 20:41:56 -0300

    att3

--- aulaDev1/treasure/exemplo/models/passaport.py
+++ aulaDev1/treasure/exemplo/models/passaport.py
@@ -2,8 +2,15 @@ from django.db import models
 from exemplo.models.person import Person
 from producao.models.base import BaseModel
 
+
 class Passaport(BaseModel):
     number = models.CharField(max_length=100, verbose_name='Passaport Nuber')
-    issue_date = models.DateField(verbose_name='Issue Date')
+    #issue_date = models.DateField(verbose_name='Issue Date')
     expiration_date = models.DateField(verbose_name='Expiration Date')
     ower = models.OneToOneField(Person, on_delete=models.CASCADE, primary_key=True)
+
+    class Meta:
+        abstract = False
+
+    def __str__(self):
+        return f'{self.number} - {self.ower.name}'
--- aulaDev1/treasure/exemplo/models/person.py
+++ aulaDev1/treasure/exemplo/models/person.py
@@ -6,4 +6,6 @@ class Person(BaseModel):
     name = models.CharField(max_length=100, verbose_name='Full name')
     birth_date = models.DateField(verbose_name='Birth Date')
     cpf = models.CharField(max_length=100, verbose_name='CPF Number')
-    
+
+    def __str__(self):
+        return f'{self.name}'
