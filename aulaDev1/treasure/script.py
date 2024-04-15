314c66a8c697d0f95bdbe1e08b628196dc53ec63
Gabriel Lisboa <gabrielcosta1055@hotmail.com>
2024-04-15 13:50:11 -0300

    finalizei o script

--- aulaDev1/treasure/script.py
+++ aulaDev1/treasure/script.py
@@ -20,9 +20,29 @@ elif opcao == 2:
     User.objects.create_superuser('ifrs', 'admin@myproject.com', 'ifrs2023')
     print("User: ifrs / Senha: ifrs2023")
 
-# 3-atualizar/cosulta
+# 3-atualizar/consulta
+elif opcao == 3:
+    flamengo = Example.objects.get(nome="Flamengo")
+    flamengo.torcedores = randint(1, 10000)  # Atualiza o número de torcedores
+    flamengo.save()  # Salva as alterações
+
+    todos_times = Example.objects.all()
+    for time in todos_times:
+        print(f"Time: {time.nome}, Torcedores: {time.torcedores}")
 
 # 4-delete delete()
+# elif opcao == 4:
+#    cruzeiro = Example.objects.get(nome="Cruzeiro")
+#    cruzeiro.delete()
+
 # 5-consultar 1 registro get()
+elif opcao == 5:
+    palmeiras = Example.objects.get(nome="Palmeiras")
+    print(f"Time: {palmeiras.nome}, Torcedores: {palmeiras.torcedores}")
+
 # 6-consultar 5 diferentes
-#teste
+elif opcao == 6:
+    import random
+    random_times = random.sample(Example.objects.all(), 5)
+    for time in random_times:
+        print(f"Time: {time.nome}, Torcedores: {time.torcedores}")
